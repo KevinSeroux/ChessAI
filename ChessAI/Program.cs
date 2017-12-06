88cf1fb796cc614aa4ede6882611af88451796fb
Kévin Seroux <kevin.seroux1@uqac.ca>
2017-12-06 13:11:54 -0500

    Remove useless variables

--- ChessAI/Program.cs
+++ ChessAI/Program.cs
@@ -60,15 +60,6 @@ namespace ChessAI
                 bool stop = false;
                 int[] tabVal = new int[64];
                 String value;
-                String[] coord = new String[] { "", "", "" };
-                String[] tabCoord = new string[] { "a8","b8","c8","d8","e8","f8","g8","h8",
-                                                "a7","b7","c7","d7","e7","f7","g7","h7",
-                                                "a6","b6","c6","d6","e6","f6","g6","h6",
-                                                "a5","b5","c5","d5","e5","f5","g5","h5",
-                                                "a4","b4","c4","d4","e4","f4","g4","h4",
-                                                "a3","b3","c3","d3","e3","f3","g3","h3",
-                                                "a2","b2","c2","d2","e2","f2","g2","h2",
-                                                "a1","b1","c1","d1","e1","f1","g1","h1" };
 
                 while (!stop)
                 {
@@ -107,7 +98,7 @@ namespace ChessAI
 
                                 board.SetFromPlatformRepresentation(tabVal);
                                 Ply ply = strategist.Run();
-                                String strPly = ply.ToString();
+                                value = ply.ToString();
 
                                 /********************************************************************************************************/
                                 /********************************************************************************************************/
@@ -115,7 +106,7 @@ namespace ChessAI
 
                                 using (var accessor = mmf2.CreateViewAccessor())
                                 {
-                                    byte[] Buffer = ASCIIEncoding.ASCII.GetBytes(strPly);
+                                    byte[] Buffer = ASCIIEncoding.ASCII.GetBytes(value);
                                     accessor.Write(0, (ushort)Buffer.Length);
                                     accessor.WriteArray(0 + 2, Buffer, 0, Buffer.Length);
                                 }
